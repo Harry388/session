54df5bbfcfedcc7ba6ab5bd8dace6f9de26ce781
Harry Thompson <harrythompson3765@gmail.com>
2026-04-28 17:16:27 +0100

    make config file if it doesn't exist

--- src/config/config.go
+++ src/config/config.go
@@ -2,9 +2,11 @@ package config
 
 import (
 	"encoding/json"
+	"errors"
 	"io"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 type Config struct {
@@ -13,8 +15,31 @@ type Config struct {
 	IncludePaths []string `json:"includePaths"`
 }
 
+const defaultConfigContent = `{
+	"searchPaths": [
+	],
+	"includePaths": [
+	]
+}`
+
 func ParseFromConfigDir(location string) (Config, error) {
 	file, err := os.Open(filepath.Join(location, "config.json"))
+	if errors.Is(err, os.ErrNotExist) {
+		err := os.MkdirAll(location, 0755) // u: rwx, g: r-x, o: r-x
+		if err != nil {
+			return Config{}, err
+		}
+		file, err = os.OpenFile(filepath.Join(location, "config.json"), os.O_CREATE|os.O_WRONLY, 0644) // u: rw-, g: r--, o: r--
+		if err != nil {
+			return Config{}, err
+		}
+		defer file.Close()
+		_, err = file.WriteString(defaultConfigContent)
+		if err != nil {
+			return Config{}, err
+		}
+		return parseConfig(strings.NewReader(defaultConfigContent))
+	}
 	if err != nil {
 		return Config{}, err
 	}
